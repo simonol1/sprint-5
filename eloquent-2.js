ae22311bde283f94d6f2a19543404c556f8b00a2
simonol1 <simondanolearyis@gmail.com>
2017-03-23 08:15:12 +1300

    Update eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,1 +1,44 @@
+//Looping a Triangle
+var hash = "#";
+for (var hash = "#"; hash.length <= 7; hash += "#") {
+console.log(hash);
+}
+#
+##
+###
+####
+#####
+######
+#######
 
+//BuzzFizz
+var number = 0
+for (var number = 0; number <= 1000; number += 1) {
+  console.log(number);
+  if (number % 3) {
+    console.log("Fizz")
+} else if(number % 5) {
+  console.log ("Buzz")
+}
+}
+0
+1
+Fizz
+2
+Fizz
+3
+Buzz
+4
+Fizz
+5
+Fizz
+6
+Buzz
+7
+Fizz
+8
+Fizz
+9
+Buzz
+
+//Chessboard
